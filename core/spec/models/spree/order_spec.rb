26492e11012854bcf30d5c545fba103c23187cff
Pau Perez <saulopefa@gmail.com>
2018-06-07 16:53:55 +0200

    Allow specifying an order_updater_decorator
    This way from OFN we can provide a class that decorates Spree's
    OrderUpdater with our custom logic.
    
    For now, the decorator provides `#before_save_hook` which enables us to
    reimplement the logic that used to contain
    `#shipping_address_from_distributor` but in a more maintainable way.

--- core/spec/models/spree/order_spec.rb
+++ core/spec/models/spree/order_spec.rb
@@ -577,5 +577,26 @@ describe Spree::Order do
       order.finalize!
     end
   end
-end
 
+  context '#updater' do
+    class FakeOrderUpdaterDecorator
+      attr_reader :decorated_object
+
+      def initialize(decorated_object)
+        @decorated_object = decorated_object
+      end
+    end
+
+    before do
+      Spree::Config.stub(:order_updater_decorator) { FakeOrderUpdaterDecorator }
+    end
+
+    it 'returns an order_updater_decorator class' do
+      order.updater.class.should == FakeOrderUpdaterDecorator
+    end
+
+    it 'decorates a Spree::OrderUpdater' do
+      order.updater.decorated_object.class.should == Spree::OrderUpdater
+    end
+  end
+end
