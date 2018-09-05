07601e6093103ff0ec4d228db853e201b83bbf3e
Hugo Daniel <hugodaniel.icam@gmail.com>
2018-09-05 15:30:25 +0200

    Configure Spree to run tests with Postgresql in Semaphore

--- core/spec/models/spree/product_spec.rb
+++ core/spec/models/spree/product_spec.rb
@@ -222,7 +222,6 @@ describe Spree::Product do
         end
 
         it "cannot create another product with the same permalink" do
-          pending '[Spree build] Failing spec'
           @product2 = create(:product, :name => 'foo')
           lambda do
             @product2.update_attributes(:permalink => @product1.permalink)
